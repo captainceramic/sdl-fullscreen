b4aa82de12ee595128811c064a3d2bbd7cf4d2d5
Tim Bedin <captainceramic@gmail.com>
2020-06-21 13:55:29 +1000

    Added the beginning of my lighting testing program

--- Makefile
+++ Makefile
@@ -2,11 +2,11 @@ export PKG_CONFIG_PATH = /opt/vc/lib/pkgconfig
 
 CC = gcc
 CXX = g++
-CFLAGS = -Wall `sdl2-config --cflags` `pkg-config brcmglesv2 --cflags` `pkg-config SDL2_image --cflags`
+CFLAGS = -Wall `sdl2-config --cflags` `pkg-config brcmglesv2 --cflags` `pkg-config SDL2_image --cflags` -I shader_loader
 LIBS = `sdl2-config --libs` `pkg-config brcmglesv2 --libs` `pkg-config SDL2_image --libs`
 
-shader_loader.o: shader_loader.c
-	$(CC) $(CFLAGS) -c shader_loader.c -o shader_loader.o
+shader_loader.o: shader_loader/shader_loader.c
+	$(CC) $(CFLAGS) -c shader_loader/shader_loader.c -o shader_loader.o
 
 opengles_fullscreen.o: opengles_fullscreen.cpp
 	$(CXX) $(CFLAGS) -c opengles_fullscreen.cpp -o opengles_fullscreen.o
---
+++ lighting_experiment/main.c
@@ -0,0 +1,88 @@
+/* A program to experiment with some of the lighting tutorials from
+   https://learnopengl.com/ and https://www.opengl-tutorial.org/
+   but ported to the pi (pre-pi4)
+
+   The plan is to set up some orbiting cubes and do some reflections and things.
+
+   The program will be written using SDL and OpenGLESv2
+
+*/
+
+#include <stdio.h>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+
+/* Global parameters */
+const int sizeX = 1920;
+const int sizeY = 1080;
+
+/* Set up global variables for the window and context. */
+SDL_Window *window;
+SDL_GLContext *glContext;
+
+/* A C struct to hold the information about our cubes
+
+   Contains:
+   * An array of vertices
+   * An array of normals
+   * A pointer to a shader program
+
+*/
+struct Cube {
+}
+
+
+void set_up() {
+  SDL_Init(SDL_INIT_VIDEO);
+  window = SDL_CreateWindow("SDL OpenGLES window",
+			    SDL_WINDOWPOS_UNDEFINED,
+			    SDL_WINDOWPOS_UNDEFINED,
+			    sizeX, sizeY,
+			    SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN);
+
+  if (window) {
+    printf("Created SDL OpenGL window\n");
+  } else {
+    printf("Error: Could not create window\n");
+  }
+
+  // Not interested in the cursor.
+  SDL_ShowCursor(SDL_DISABLE);
+
+  // Create the actual context for GLESv2
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+  glContext = SDL_GL_CreateContext(window);
+
+  if (glContext) {
+    printf("OpenGLES render information:\n");
+    printf("\tOpenGLES version: %s\n", glGetString(GL_VERSION));
+    printf("\tVendor: %s\n", glGetString(GL_VENDOR));
+    printf("\tRenderer: %s\n", glGetString(GL_RENDERER));
+    printf("\tShading Language Version: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+  } else {
+    printf("Error: Could not create OpenGL context\n");
+  }
+}
+
+void clean_up() {
+  SDL_GL_DeleteContext(glContext);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+}
+
+int main(int argc, char* argv[]) {  
+  set_up();
+
+
+
+
+
+  clean_up();
+
+  return 0;
+}
+

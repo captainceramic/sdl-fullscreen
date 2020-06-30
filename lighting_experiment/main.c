cb822a9ad81ddadbbcd07f9947b8c36cc14c23f6
Tim Bedin <captainceramic@gmail.com>
2020-06-30 18:23:47 +1000

    Semi-working version

--- lighting_experiment/main.c
+++ lighting_experiment/main.c
@@ -118,22 +118,24 @@ void destroy_cube(Cube * thisCube) {
 }
 
 void create_view_matrix(mat4* view_matrix_ptr) {
-  /* This is a function from
-     https://www.3dgep.com/understanding-the-view-matrix/ 
-  */
+  vec3 eye = GLM_VEC3_ZERO_INIT;
+  eye[2] = 8.0;
+  eye[1] = 0.5;
+  
+  vec3 centre = GLM_VEC3_ZERO_INIT;
 
-  mat4 orientation = GLM_MAT4_IDENTITY_INIT;
-  mat4 translation = GLM_MAT4_IDENTITY_INIT;
-
-  /* Eye location is looking from the y axis, 5 units*/
-  translation[1][3] = -5.0f;
-
-  /* do the matrix multiplication */
-  glm_mat4_mul(orientation, translation, *view_matrix_ptr);
+  vec3 up = GLM_VEC3_ZERO_INIT;
+  up[1] = 1.0;
+  
+  glm_lookat(eye, centre, up, *view_matrix_ptr);
 }
 
 void create_projection_matrix(mat4* projection_matrix_ptr) {
   glm_mat4_identity(*projection_matrix_ptr);
+  glm_perspective(glm_rad(45.0f),
+		  (float) sizeX / (float) sizeY,
+		  0.1f, 100.0f,
+		  *projection_matrix_ptr);
 }
 
 int main(int argc, char* argv[]) {  
@@ -141,9 +143,10 @@ int main(int argc, char* argv[]) {
   set_up();
 
   Cube cube_1 = create_cube("../data/cube.obj");
-  /* Cube cube_2 = create_cube("../data/cube.obj"); */
+  Cube cube_2 = create_cube("../data/cube.obj");
 
-  /* Next up: we need view and projection matrices */
+  /* Next up: we need view and projection matrices
+     these stay constant */
   mat4 view_matrix = GLM_MAT4_IDENTITY_INIT;
   create_view_matrix(&view_matrix);
 
@@ -154,28 +157,29 @@ int main(int argc, char* argv[]) {
      and assign it to the relevant cubes */
   GLuint basicShader = load_shaders(vertexShaderPath, fragmentShaderPath);
   cube_1.shaderProgramAddress = basicShader;
-  /* cube_2.shaderProgramAddress = basicShader; */
+  cube_2.shaderProgramAddress = basicShader;
 
-  /* Get the location of the vPosition attribute in the shader program
-     currently just for shader 1.
-   */
-  GLint position_attr_i = glGetAttribLocation(cube_1.shaderProgramAddress, "vPosition");
-  GLint model_mat_i = glGetAttribLocation(cube_1.shaderProgramAddress, "mat_model");
-  GLint view_mat_i = glGetAttribLocation(cube_1.shaderProgramAddress, "mat_view");
-  GLint projection_mat_i = glGetAttribLocation(cube_1.shaderProgramAddress, "mat_projection");
+  /* Get the location of the vPosition attribute in the shader program */
+  GLint position_attr_i = glGetAttribLocation(basicShader, "vPosition");
 
-  /* Let's check that our model matrix is working */
-  for(int i=0; i<4; i++) {
-    for(int j=0; j<4; j++) {
-      printf("\t%f\t", cube_1.model_matrix[i][j]);
-    }
-    printf("\n");
-  }
- 
+  /* Get the location for the uniforms */
+  GLint model_ix = glGetUniformLocation(basicShader, "model");
+  GLint view_ix = glGetUniformLocation(basicShader, "view");
+  GLint perspective_ix = glGetUniformLocation(basicShader, "perspective");
 
+  /* Set up the vertex buffer for cube 1 */
+  glGenBuffers(1, &cube_1.vertexVBO);
+  glBindBuffer(GL_ARRAY_BUFFER, cube_1.vertexVBO);
+  // Send the vertices to OpenGL.
+  glBufferData(GL_ARRAY_BUFFER,
+	       cube_1.num_triangles * 3 * 3* sizeof(float),
+	       cube_1.vertices,
+	       GL_STATIC_DRAW);
+  
   /* Now - a main animation loop! */
   bool shouldExit = false;
   SDL_Event event;
+
   while(!shouldExit) {
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -187,32 +191,42 @@ int main(int argc, char* argv[]) {
       }
     }
    
-    /* Render cube 1 */
+    /* Render cube 1 */     
     glUseProgram(cube_1.shaderProgramAddress);
+
+    glEnableVertexAttribArray(position_attr_i);
     glBindBuffer(GL_ARRAY_BUFFER, cube_1.vertexVBO);
     glVertexAttribPointer(position_attr_i, 3,
-    			  GL_FLOAT, GL_FALSE,
-    			  3*sizeof(GLfloat),
-    			  (void*) (0*sizeof(GLfloat)));
-    glEnableVertexAttribArray(position_attr_i);
+			  GL_FLOAT, GL_FALSE,
+			  3*sizeof(GLfloat),
+			  (void*)(0*sizeof(GLfloat)));
+
+    /* Apply a rotation to the cube_1 model matrix */
+    vec3 rotAxis = GLM_VEC3_ZERO_INIT;
+    rotAxis[1] = 1.0f;
+    rotAxis[0] = 0.2f;
+    glm_rotate(cube_1.model_matrix,
+	       0.05f,
+	       rotAxis);
 
     /* Next: apply the model, view and projection matrices
        Plan: send the M, V and P matrices into the shaders */
-    glUniformMatrix4fv(model_mat_i, 1, GL_FALSE, (float *)cube_1.model_matrix);
-    glUniformMatrix4fv(view_mat_i, 1, GL_FALSE, view_matrix[0]);
-    glUniformMatrix4fv(projection_mat_i, 1, GL_FALSE, projection_matrix[0]);
-    
-    glDrawArrays(GL_TRIANGLES, 0, cube_1.num_triangles);
-	
-    SDL_GL_SwapWindow(window);
-    
+    glUniformMatrix4fv(model_ix, 1, GL_FALSE, cube_1.model_matrix[0]);
+    glUniformMatrix4fv(view_ix, 1, GL_FALSE, &view_matrix[0][0]);
+    glUniformMatrix4fv(perspective_ix, 1, GL_FALSE, &projection_matrix[0][0]);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3 * cube_1.num_triangles);
+     
     glDisableVertexAttribArray(position_attr_i);
 
+    /* Rendering cube 2 will happen here */
+    SDL_GL_SwapWindow(window);
+   
   }
  
   /* Clean up functions */
   destroy_cube(&cube_1);
-  /* destroy_cube(&cube_2); */
+  destroy_cube(&cube_2);
   clean_up();
 
   return 0;
--- lighting_experiment/shaders/shader.vert
+++ lighting_experiment/shaders/shader.vert
@@ -1,11 +1,11 @@
 #version 100
 
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 perspective;
+
 attribute vec3 vPosition;
-uniform mat4 mat_model;
-uniform mat4 mat_view;
-uniform mat4 mat_projection;
 
 void main() {
-  /* gl_Position = mat_model * mat_view * mat_projection * vec4(vPosition, 1.0); */
-  gl_Position = vec4(vPosition, 1.0);
+  gl_Position = perspective * view * model * vec4(vPosition, 1.0);
 }
